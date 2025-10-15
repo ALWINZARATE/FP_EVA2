a446401ec6a48099d4f74d00c3a05219f3100b7a
ALWINZARATE <L25550655@chihuahua2.tecnm.mx>
2025-10-15 17:50:25 -0600

    Add files via upload

---
+++ EVA2_19_TIC_TAC_TOE/src/eva2_19_tic_tac_toe/EVA2_19_TIC_TAC_TOE.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_19_tic_tac_toe;
+
+/**
+ *
+ * @author zarat
+ */
+public class EVA2_19_TIC_TAC_TOE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
